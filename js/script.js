6bb21ea95a1689e6ddd02534d16e0c2da44f22c5
IdoppelTI <38787002+IdoppelTI@users.noreply.github.com>
2018-09-03 20:46:43 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,41 @@
+var ls = localStorage.getItem('namespace.visited');
+if (ls == null) {
+  localStorage.setItem('namespace.visited', 1);
 
-window.addEventListener("load", function(){
+  window.addEventListener("load", function(){
+    setTimeout(function() {
+      document.getElementById("loader").style.height = "0px";
 
-  setTimeout(function() {
-    document.getElementById("loader").style.height = "0px";
+      document.getElementById("loader").style.top = "100vh";
 
-    document.getElementById("loader").style.top = "100vh";
+      document.getElementById("loader").style.visibility = "hidden";
 
-    document.getElementById("loader").style.visibility = "hidden";
+      setTimeout(function() {
+        document.getElementById("transitionColor").style.height = "0px";
+
+        document.getElementById("transitionColor").style.top = "100vh";
+
+        document.getElementById("transitionColor").style.visibility = "hidden";
+      }, 250); //250
+  }, 2750);    //2750
+  });
+}else{
+  window.addEventListener("load", function(){
 
     setTimeout(function() {
-      document.getElementById("transitionColor").style.height = "0px";
+      document.getElementById("loader").style.height = "0px";
 
-      document.getElementById("transitionColor").style.top = "100vh";
+      document.getElementById("loader").style.top = "100vh";
 
-      document.getElementById("transitionColor").style.visibility = "hidden";
-    }, 250); //250
-}, 0);    //2750
-});
+      document.getElementById("loader").style.visibility = "hidden";
+
+      setTimeout(function() {
+        document.getElementById("transitionColor").style.height = "0px";
+
+        document.getElementById("transitionColor").style.top = "100vh";
+
+        document.getElementById("transitionColor").style.visibility = "hidden";
+      }, 250); //250
+  }, 0);    //2750
+  });
+}
